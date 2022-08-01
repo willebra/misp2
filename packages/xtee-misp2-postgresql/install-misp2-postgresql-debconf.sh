e5a2b4198be44f113b39b72c0eaba7a126a2d7a9
Raido Kaju <raido.kaju@gmail.com>
2022-08-01 11:06:02 +0300

    Chore: migrate missing fixes + correct database update version

--- packages/xtee-misp2-postgresql/install-misp2-postgresql-debconf.sh
+++ packages/xtee-misp2-postgresql/install-misp2-postgresql-debconf.sh
@@ -24,7 +24,7 @@ pgsql_default_port=5432
 pgsql_default_dbname=misp2db
 pgsql_dir=/usr/lib/postgresql/10/bin
 pgsql_conf_dir=/etc/postgresql/10/main
-current_version="2.6.3"
+current_version="2.7.0"
 
 
 error_prefix="\e[1m\e[91mERROR!\e[0m"
@@ -397,6 +397,14 @@ else
     		version="2.6.3"
     fi
 
+	if [ "$version" == "2.6.3" ]
+    	then
+    		version="2.6.4"
+    fi
+	if [ "$version" == "2.6.4" ]
+    	then
+    		version="7.0.0"
+    fi
 	# Substitute schema name in alter scripts
 	perl -pi -e "s/<misp2_schema>/$schema_name/g" $workdir/tmp.alter.sql
 	
